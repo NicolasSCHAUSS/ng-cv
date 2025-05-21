b6465a13a63a4b74e31312a18fc17aa987e06e43
NicolasSCHAUSS <nicolas.schauss@gmail.com>
2025-05-21 19:00:59 +0200

    style for formation component

--- src/app/component/formation/formation.component.ts
+++ src/app/component/formation/formation.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { TimelineModule } from 'primeng/timeline';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { Formation } from '../../model/formation';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-formation',
-  imports: [],
+  imports: [TimelineModule, CardModule, ButtonModule],
   templateUrl: './formation.component.html',
   styleUrl: './formation.component.css'
 })
 export class FormationComponent {
+  protected formations: Array<Formation> = [];
 
+  constructor(private http: HttpClient) {
+    this.http.get<Array<Formation>>('/assets/formations.json').subscribe((data) => {
+      this.formations = data;
+    });
+  }
 }
---
+++ src/app/model/formation.ts
@@ -0,0 +1,7 @@
+export interface Formation {
+    institution: string
+    year: number
+    title: string
+    town: string
+    icon: string
+}
