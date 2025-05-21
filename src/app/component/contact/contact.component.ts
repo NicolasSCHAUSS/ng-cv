af86a73ca0a566cb4c4959c72cba9bd310c470f9
NicolasSCHAUSS <nicolas.schauss@gmail.com>
2025-05-21 19:57:09 +0200

    style for contact component

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Divider } from 'primeng/divider';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [Divider],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  protected linkedin: string = 'https://www.linkedin.com/in/nicolas-schauss-637bb2206/';
+  protected discord: string = 'https://discord.com/users/ixionfire';
+  protected facebook: string = 'https://www.facebook.com/ixionfire';
+  protected email: string = 'nicolas.schauss@gmail.com';
+  protected phone: string = '+33 6 63 22 01 86';
+  protected localisation: string = 'Carcassonne, France';
+
+  constructor() { }
 }
